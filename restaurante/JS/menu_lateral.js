7e12dbfd1f9af5c4a55af9b28a5f4b476a4535b1
MiKaelchrist <131804096+MiKaelchrist@users.noreply.github.com>
2023-11-30 16:09:32 -0300

    Delete restaurante directory

--- restaurante/JS/menu_lateral.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.cabecalho__icone__menu')
-const menuLateral = document.querySelector('.menu__lateral')
-
-botaoMenu.addEventListener('click', () => {
-    menuLateral.classList.toggle('menu__lateral--ativo')
-})
